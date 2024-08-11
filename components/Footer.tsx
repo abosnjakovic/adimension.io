d9e93657c474bbc1f430a6a054bb0a1329729f41
Adam Bosnjakovic <adam@adimension.io>
2024-08-11 15:35:19 +1000

    fix: remove the now playing from footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,6 @@ export default function Footer() {
   return (
     <footer>
       <div className="mb-0 flex flex-col justify-start space-y-1.5 space-x-0 py-10 text-gray-500 dark:text-gray-400">
-        <NowPlaying />
         <div className="flex flex-col items-center space-y-2 text-sm sm:flex-row sm:justify-between sm:text-base">
           <ul className="flex space-x-2">
             <li>{`© ${new Date().getFullYear()}`}</li>
